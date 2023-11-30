3176918c2ec3028ae6b2d2970c6d9c91203b4a80
Eirik Habbestad Helleve <eirik-habbestad.helleve@telenor.no>
2023-11-30 01:04:19 +0100

    adding json config for connecting to netbox and error handling for url var and api token var

---
+++ vmdeploy-webclient.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import sys
+import os
+import json
+import pynetbox
+
+file = '.nb_config.json'
+
+path = os.path.abspath(f'{file}')
+
+n = open(f'{path}')
+
+data = json.load(n)
+
+nb_url = data['url']
+nb_api_token = data['token']
+
+if not nb_url:
+    print("netbox url variable nb_url is not set")
+    sys.exit()
+
+if not nb_api_token:
+    print("netbox api token variable nb_api_token is not set")
+
+
+nb = pynetbox.api(
+    f'{nb_url}',
+    token=f'{nb_api_token}'
+)
