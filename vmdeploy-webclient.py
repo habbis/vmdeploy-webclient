fe4230f6928c3753b39b3762b3ef8bf10c736446
Eirik Habbestad Helleve <eirik-habbestad.helleve@telenor.no>
2023-12-10 21:43:45 +0100

    changes

--- vmdeploy-webclient.py
+++ vmdeploy-webclient.py
@@ -1,33 +1,96 @@
 #!/usr/bin/env python3
-import sys
-import os
-import json
-import pynetbox
+#import sys
+#import os
+import MySQLdb as mysql
+import streamlit as st
 
-file = '.nb_config.json'
+#file = '.my.cnf'
 
-path = os.path.abspath(f'{file}')
+#path = os.path.abspath(f'{file}')
 
-n = open(f'{path}')
+db = 'infra'
 
-data = json.load(n)
-
-nb_url = data['url']
-nb_api_token = data['token']
-
-if not nb_url:
-    print("netbox url variable nb_url is not set")
-    sys.exit()
-
-if not nb_api_token:
-    print("netbox api token variable nb_api_token is not set")
+#mydb = mysql.connect(database=f"{db}", read_default_file=f"{path}")
+mydb = mysql.connect(database=f"{db}", read_default_file=f"~/.my.cnf")
 
 
-nb = pynetbox.api(
-    f'{nb_url}',
-    token=f'{nb_api_token}'
-)
+c = mydb.cursor()
 
-#response = nb.ipam.vlans.get()
 
-#print(response)
+
+#def virt_host_query():
+    #SELECT cluster_name FROM cluster;
+#    c.execute("SELECT virt_host FROM cluster")
+#    records = c.fetchall()
+#    if not records:
+#       print("check your the virt host query")
+#       sys.exit()
+#    for r in records:
+#      print (r[0])
+
+c.execute("SELECT virt_host FROM cluster")
+records1 = c.fetchall()
+if not records1:
+  print("check your the virt host query")
+  sys.exit()
+for i in records1:
+  #print (r[0])
+  test = i[0]
+
+
+
+#def cluster_list_query():
+#    #SELECT cluster_name FROM cluster;
+#    c.execute("SELECT cluster_name FROM cluster")
+#    records = c.fetchall()
+#    if not records:
+#       print("check your the cluster query")
+#       sys.exit()
+#    for r in records:
+#      print (r[0])
+
+c.execute("SELECT cluster_name FROM cluster")
+records2 = c.fetchall()
+#records2 = c.fetchone()
+if not records2:
+  print("check your the cluster query")
+  sys.exit()
+#for r in records2:
+  #print (r[0])
+#  test2 = r[0]
+test2 = records2[0]
+test3 = records2[1]
+test4 = records2[2]
+#print(test2)
+#print(test3)
+#print(test4)
+
+
+
+
+
+#st.title("Deploy VM")
+
+#option_cluster =  st.selectbox("Select virt host",[f"{test2}",f"{test3}",f"{test4}"])
+
+
+#option_cluster 
+
+
+#cluster_list()
+#virt_host_list()
+
+
+
+
+
+try:
+    commit = mydb.commit()
+except Exception as error:
+    print("Trying to commit query somting happend", error)
+
+try:
+    c.close()
+    mydb.close()
+except:
+    print("Trying to close database connection somthing went wrong")
